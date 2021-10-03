9d355f198491d9fb49bb3b9781ea9526e226f812
ozgrmrs <ozgrmrs@gmail.com>
2021-10-03 18:14:07 +0300

    örnek

--- Javascript/Ders2/App.js
+++ Javascript/Ders2/App.js
@@ -1,1 +1,11 @@
 console.log('I am outside');
+var quantity;
+quantity=3;
+console.log(quantity);
+var a = 5;
+console.log(a);
+var alert = 5;
+console.log(alert);
+console.log(typeof alert)
+
+alert('merhaba class')
---
+++ Javascript/Ders2/App2.js
@@ -0,0 +1,4 @@
+console.log('hello world')
+var x = 0.1+0.2;
+console.log(x);
+
